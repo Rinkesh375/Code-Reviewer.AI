d05e0063a9d91b965e693fa5d4f798039478251c
Rinkesh375 <rinkeshujjwal16@gmail.com>
2026-01-18 10:53:19 +0530

    fix build error

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-
+// Initialize the Prisma Client with PostgreSQL adapter
 const adapter = new PrismaPg({
   connectionString: process.env["DATABASE_URL"]!,
 });

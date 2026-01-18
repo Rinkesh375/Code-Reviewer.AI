9a9726de56086b4f65505f7fa2467b67e36d984d
Rinkesh375 <rinkeshujjwal16@gmail.com>
2026-01-18 18:08:16 +0530

    fixed inngest route error

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,3 +1,6 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "Code Reviewer.AI" });
+export const inngest = new Inngest({
+  id: "Code Reviewer.AI",
+  eventKey: process.env.INNGEST_EVENT_KEY,
+});
